4e4e850ea1eafb194fdfa00193d00050bf0e7980
James Brink <brink.james@gmail.com>
2019-11-01 22:27:54 -0700

    Fix entrypoint - add pid file cleanup

--- runtime-assets/usr/local/bin/entrypoint.sh
+++ runtime-assets/usr/local/bin/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 if [ "$1" = "vbmcd" ]; then
+	# Clean up old pid files.
+	if [ -f "/virtualbmc/.vbmc/master.pid" ];then
+		echo "Removing old PID file."
+		rm -f /virtualbmc/.vbmc/master.pid
+	fi
 	echo "Starting VirtualBMC daemon"
 	/usr/local/bin/vbmcd --foreground &
 	trap 'echo "Stopping VirtualBMC daemon"; kill -SIGTERM "$!"' SIGINT SIGTERM
